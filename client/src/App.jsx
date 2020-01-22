c3ea776cd5905b69dd8aceb738b66e8b2c43114f
Andreas <andrewhoj@gmail.com>
2020-01-22 10:11:52 +0100

    will it work?

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,66 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
-import RegisterForm from "./components/RegisterForm";
-import Secret from "./components/Secret";
 import UserContextProvider from "./contexts/UserContext";
 import LoggedButton from "./components/LoggedButton";
+import styled from "styled-components";
+import logo from "./assets/devdash.svg";
+import { useCookies, Cookies } from "react-cookie";
 
 function App() {
-  return (
-    <Router>
-      <UserContextProvider>
-        <nav>
-          <Link to="/">Dashboard</Link>
-          <Link to="/secret">Secret</Link>
-          <LoggedButton />
-        </nav>
+  const [background, setBackground] = useState();
+  const [cookies, setCookies, removeCookies] = useCookies(["background"]);
+  const cookie = new Cookies();
 
-        <Switch>
-          <Route path="/secret">
-            <Secret />
-          </Route>
-          <Route path="/">
-            <RegisterForm />
-          </Route>
-        </Switch>
-      </UserContextProvider>
-    </Router>
+  const getBackground = () => {
+    const backgroundURL = cookie.get("background-cookie");
+    console.log(background, backgroundURL);
+    const tomorrow = new Date();
+    tomorrow.setDate(new Date().getDate() + 1);
+    if (backgroundURL !== undefined) {
+      setBackground(backgroundURL);
+    } else {
+      fetch("https://source.unsplash.com/1600x900/?background,relax").then(
+        background => {
+          setBackground(background.url);
+          setCookies("background-cookie", background.url, {
+            expires: tomorrow
+          });
+        }
+      );
+    }
+  };
+
+  useEffect(() => {
+    getBackground();
+    console.log(background);
+  }, []);
+
+  return (
+    <UserContextProvider>
+      <Container background={background}>
+        <div className="first-row">
+          <img src={logo} alt="Dev dash logo" />
+        </div>
+        <LoggedButton />
+      </Container>
+    </UserContextProvider>
   );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-image: url('${props => props.background}');
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  .first-row {
+    width: 90%;
+    img {
+      opacity: 0.8;
+    }
+  }
+`;
 
 export default App;
